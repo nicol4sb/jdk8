0ee04395445f7979eba4c97bf7841c5fbfb745e4
nicol4sb <nicolasb@gmail.com>
2020-06-15 09:37:00 +0200

    Basic usage of streams and lambdas

---
+++ TestJDK8.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Arrays;
+
+public class TestJDK8 {
+    public static void main(String args[]) {
+        
+        // read all non hidden files
+        Arrays.stream(new File(".").listFiles(f -> !f.isHidden())).forEach(file -> System.out.println(file));
+
+    }
+}
