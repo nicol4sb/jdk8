c59a6e173b7dfda773ce0aa157f20bc8e3902e0b
nicol4sb <nicolasb@gmail.com>
2020-06-15 13:41:18 +0200

    Refine perf check and demonstration on multicore

--- TestJDK8.java
+++ TestJDK8.java
@@ -4,10 +4,20 @@ import java.util.Arrays;
 public class TestJDK8 {
     public static void main(String args[]) {
         
+        // let's do some execution perf checks
+        long startTime = System.nanoTime();
+
         // read all non hidden files
         Arrays.stream(new File(".").listFiles(f -> !f.isHidden())).forEach(file -> System.out.println(file));
+        long midTime = System.nanoTime();
 
-        
+        // convert to a List so Collection thus stream() is available
+        // note that this one uses parallelStream - makes it consistently twice faster - on my dual core
+        Arrays.asList(new File(".").listFiles(f -> !f.isHidden())).parallelStream()
+                .forEach(file -> System.out.println(file));
 
+        long endTime = System.nanoTime();
+
+        System.out.println("1 : "+(midTime-startTime+"\n2 : "+(endTime-midTime)));
     }
 }
